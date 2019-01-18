b5acf9d35d4b2fd0733f383ce04f1395b7a27586
Alchemy <2457043027@qq.com>
2019-01-18 18:53:29 +0800

    新增.gitignore文件 增加注释

---
+++ src/me/first/project/Main.java
@@ -0,0 +1,34 @@
+package me.first.project;
+
+/**
+ * @author ziheng kejun
+ * @date 2019/1/18
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+         * 变量定义遵循
+         * 修饰符 变量类型 变量 = 赋值 ;
+         */
+        String yourmotherboom = "你妈也死了";
+
+        int second = 2;
+
+        double onepoinone = 1.1;
+
+        /*
+         * 打印变量内容，输出结果为你妈也死了
+         */
+        System.out.println(yourmotherboom);
+
+        /*
+         * 将second转换为String类型
+         */
+        info(String.valueOf(second));
+    }
+
+    private static void info(String info){
+        System.out.println(info);
+    }
+}
